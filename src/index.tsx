19f7068cde269b20924c872c41309831f2a82de4
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2023-09-06 00:51:28 +0900

    add Hello Components

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+// React.StrictMode: アプリケーションの潜在的な問題点を洗い出すためのツール
   <React.StrictMode>
+    {/* App Component*/}
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

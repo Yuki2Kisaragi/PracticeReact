1008c23604550e264ec03183e7d80e290572b156
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2023-09-09 21:05:11 +0900

    Learn React's Type and props.

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -1,4 +1,11 @@
-const Container = (props: {title: string, children: React.ReactElement }) => {
+import React from "react"
+
+type ContainerProps = {
+    title:string,
+    children:React.ReactNode
+}
+
+const Container = (props: ContainerProps): JSX.Element => {
 
     const { title, children } = props
 
@@ -10,7 +17,7 @@ const Container = (props: {title: string, children: React.ReactElement }) => {
     )
 }
 
-const Parent = () => {
+const Parent = (): JSX.Element => {
     return ( 
         <Container title="Hello">
             <p>This area is arounded backgroundColor</p>
